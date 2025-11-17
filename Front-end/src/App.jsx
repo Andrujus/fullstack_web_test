2ba295d19aac8890a7977046cf63719a8cb2aaf3
Justas Andru <justasandrusk@gmail.com>
2025-11-17 23:59:47 +0200

    favorites and home migrations

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,13 +1,19 @@
 import MovieCard from "./components/movie_card"
+import Favorite from "./migrations/Favorites"
 import Home from "./migrations/Home"
+import { Route, Routes } from "react-router-dom"
 import './App.css'
 
 function App() {
-return(
-  <>
-    <Home></Home>
-  </>
-)}
+return (
+  <main className="main-content">
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/favorites" element={<Favorite/>}/>
+    </Routes>
+  </main>
+  );
+}
 
 
 export default App
---
+++ Front-end/src/migrations/Favorites.jsx
@@ -0,0 +1,11 @@
+
+function Favorite(){
+    return (
+        <div className="fav-empty">
+            <h2>No favorite movies yet</h2>
+            <p>starts liking movies for them to appear here.</p>
+        </div>
+    )
+}
+
+export default Favorite
